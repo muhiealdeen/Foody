43b8a75d8dc12806cfbb0a742456f33fa57cd65d
Muhie Aldeen <zezo1998@hotmail.com>
2023-08-11 18:22:02 +0200

    changed the the content of the links in the navbar

--- middle-eastern-delights-foody-app/src/components/NavBar.js
+++ middle-eastern-delights-foody-app/src/components/NavBar.js
@@ -6,8 +6,8 @@ const NavBar = () => {
       <div>Logo</div>
       <input type="text" placeholder="Search recipes..." />
       <ul>
-        <li>Link 1</li>
-        <li>Link 2</li>
+        <li>About the app </li>
+        <li>Contct us</li>
       </ul>
     </nav>
   );
