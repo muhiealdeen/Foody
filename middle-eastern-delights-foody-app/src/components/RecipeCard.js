76114fb1073b62fda3d388735820c67bf2f6f3d9
Muhie Aldeen <zezo1998@hotmail.com>
2023-08-13 23:10:24 +0200

    I finished the search fuction and created the about page and added content to the home page and made some changes in the css file Created an assets folder to store the imgs and added some css properties still try to solve the problem of doublicated search results

--- middle-eastern-delights-foody-app/src/components/RecipeCard.js
+++ middle-eastern-delights-foody-app/src/components/RecipeCard.js
@@ -5,13 +5,22 @@ import '../App.css';
 const RecipeCard = ({ recipe }) => {
   const navigate = useNavigate();
 
+  console.log('>>>>>>>>> ', recipe.image);
+
   const handleCardClick = () => {
     navigate(`/recipe/${recipe.id}`);
   };
   return (
     <div className="recipe-card" onClick={handleCardClick}>
       <div>
-        <img src={recipe.image} alt={recipe.name} />
+        <img
+          src={
+            recipe.image !== ''
+              ? recipe.image
+              : '../assets/pngtree-vector-illustration-knife-and-fork-western-food-plate-image_2283844.jpg'
+          }
+          alt={recipe.name}
+        />
       </div>
       <p>{recipe.name}</p>
     </div>
--- middle-eastern-delights-foody-app/src/pages/RecipeDetailsPage.js
+++ middle-eastern-delights-foody-app/src/pages/RecipeDetailsPage.js
@@ -42,7 +42,9 @@ const RecipeDetailsPage = () => {
               </li>
             ))}
           </ul>
-          <p className="calories">Total Calories: {selectedRecipe.calories}</p>
+          <p className="calories">
+            Total Calories: {selectedRecipe.calories.toFixed(2)}
+          </p>
           <a className="more-info" href={selectedRecipe.moreInfo}>
             More Information
           </a>
